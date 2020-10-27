2cd34a36cfca10cb5b17ddb963ee3d9e59ed7f70
ixnewton <i.newton@c-org.com>
2020-10-27 00:08:42 +0000

    update parameter order windowmove_L.sh

--- usr/local/bin/windowmove_L.sh
+++ usr/local/bin/windowmove_L.sh
@@ -6,7 +6,7 @@ window_name=$(xdotool getwindowname $active_window_id)
 display_height=$(xdotool getdisplaygeometry | awk -F "[[:space:]]+" '/ /{print $2}')
 window_width=$(xdotool getwindowgeometry $active_window_id | awk -F "[[:space:]x]+" '/Geometry:/{print $3}')
 window_height=$(xdotool getwindowgeometry $active_window_id | awk -F "[[:space:]x]+" '/Geometry:/{print $4}')
-window_fit_height=$(($display_height - $2 - $3))
+window_fit_height=$(($display_height - $1 - $3))
 
 
 if [ "$window_name" != "Desktop — Plasma" ]; then
@@ -14,5 +14,5 @@ if [ "$window_name" != "Desktop — Plasma" ]; then
         window_new_height=$window_fit_height
         xdotool windowsize $active_window_id $window_width $window_new_height
     fi
-    xdotool windowmove $active_window_id $1 $2
+    xdotool windowmove $active_window_id $2 $1
 fi
