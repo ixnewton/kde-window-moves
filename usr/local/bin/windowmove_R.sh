82b7f1724cfc5eaa5aad9883da7531105efd22d8
ixnewton <i.newton@c-org.com>
2020-10-28 09:07:30 +0000

    update parameter order windowmove_R.sh

--- usr/local/bin/windowmove_R.sh
+++ usr/local/bin/windowmove_R.sh
@@ -6,8 +6,8 @@ display_width=$(xdotool getdisplaygeometry | awk -F "[[:space:]]+" '/ /{print $1
 display_height=$(xdotool getdisplaygeometry | awk -F "[[:space:]]+" '/ /{print $2}')
 window_width=$(xdotool getwindowgeometry $active_window_id | awk -F "[[:space:]x]+" '/Geometry:/{print $3}')
 window_height=$(xdotool getwindowgeometry $active_window_id | awk -F "[[:space:]x]+" '/Geometry:/{print $4}')
-window_x_pos=$(($display_width - $window_width - $3))
-window_fit_height=$(($display_height - $1 - $2))
+window_x_pos=$(($display_width - $window_width - $2))
+window_fit_height=$(($display_height - $1 - $3))
  window_y_pos=$1
 
 if [ "$window_name" != "Desktop — Plasma" ];
